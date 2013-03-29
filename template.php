2fc43e7f00694dae05aa10addf5cc1901287df48
Mike Pirog <mike@kalamuna.com>
2013-03-29 23:33:40 +0000

    submenus show on hover, parent is now link not data toggle

--- template.php
+++ template.php
@@ -92,21 +92,114 @@ function openasu_bootstrap_preprocess_block(&$variables) {
 }
 
 /**
+ * Implements theme_links__system_main_menu.
+ */
+function openasu_bootstrap_links__system_main_menu($variables) {
+  $links = $variables['links'];
+  $attributes = $variables['attributes'];
+  $heading = $variables['heading'];
+  unset($links['#sorted']);
+  unset($links['#theme_wrappers']);
+  global $language_url;
+  $output = '';
+
+  if (count($links) > 0) {
+    $output = '';
+
+    // Treat the heading first if it is present to prepend it to the
+    // list of links.
+    if (!empty($heading)) {
+      if (is_string($heading)) {
+        // Prepare the array that will be used when the passed heading
+        // is a string.
+        $heading = array(
+          'text' => $heading,
+          // Set the default level of the heading.
+          'level' => 'h2',
+        );
+      }
+      $output .= '<' . $heading['level'];
+      if (!empty($heading['class'])) {
+        $output .= drupal_attributes(array('class' => $heading['class']));
+      }
+      $output .= '>' . check_plain($heading['text']) . '</' . $heading['level'] . '>';
+    }
+
+    $output .= '<ul' . drupal_attributes($attributes) . '>';
+
+    $num_links = count($links);
+    $i = 1;
+
+    foreach ($links as $key => $link) {
+      $class = array($key);
+
+      // Add first/last/active classes to help out themers.
+      if ($i == 1) {
+        $class[] = 'first';
+      }
+      if ($i == $num_links) {
+        $class[] = 'last';
+      }
+      if (isset($link['#href']) && ($link['#href'] == $_GET['q'] || ($link['#href'] == '<front>' && drupal_is_front_page()))
+        && (empty($link['#language']) || $link['#language']->language == $language_url->language)) {
+        $class[] = 'active';
+      }
+      if (!empty($link['#below'])) {
+        $class[] = 'dropdown';
+        //$link['#attributes']['data-toggle'] = 'dropdown';
+        $link['#attributes']['class'][] = 'dropdown-toggle';
+        //$link['#href'] = NULL;
+      }
+      $options['attributes'] = $link['#attributes'];
+
+      $output .= '<li' . drupal_attributes(array('class' => $class)) . '>';
+
+      if (isset($link['#href'])) {
+        // Pass in $link as $options, they share the same keys.
+        $output .= l($link['#title'], $link['#href'], array('attributes' => $link['#attributes']));
+      }
+      // Put in empty anchor for dropdown.
+      elseif ($link['#attributes']['data-toggle'] && !isset($link['#href'])) {
+        $output .= l($link['#title'], $link['#href'], array('attributes' => $link['#attributes']));
+      }
+      elseif (!empty($link['#title'])) {
+        // Wrap non-<a> links in <span> for adding title and class attributes.
+        if (empty($link['#html'])) {
+          $link['#title'] = check_plain($link['#title']);
+        }
+        $span_attributes = '';
+        if (isset($link['#attributes'])) {
+          $span_attributes = drupal_attributes($link['#attributes']);
+        }
+        $output .= '<span' . $span_attributes . '>' . $link['#title'] . '</span>';
+      }
+
+      if (!empty($link['#below'])) {
+        $output .= theme('links__system_main_menu', array(
+          'links' => $link['#below'],
+          'attributes' => array(
+            'class' => array('dropdown-menu'),
+          ),
+        ));
+      }
+
+      $i++;
+      $output .= "</li>\n";
+    }
+
+    $output .= '</ul>';
+  }
+
+  return $output;
+}
+
+/**
  * Implements hook_form_FORM_ID_alter().
  */
 function openasu_bootstrap_form_panels_edit_style_settings_form_alter(&$form, &$form_state) {
   // Add some extra ASU styles if extra styles are on
   if (isset($form['general_settings']['settings']['title'])) {
     $styles = array('title', 'content');
-    foreach ($styles as $style) {
-      $form['general_settings']['settings'][$style]['attributes']['#options'] += array(
-        'featured-text' => 'ASU FEATURED TEXT',
-      );
-    }
-  }
-}
-
- 'content');
     foreach ($styles as $style) {
       $form['general_settings']['settings'][$style]['attributes']['#options'] += array(
         'featured-text' => 'ASU FEATURED TEXT',
